8a15f1482cd5cc3573786820948d0b1b9df14217
Rob Eisenberg <rob@bluespire.com>
2013-01-25 11:02:22 -0500

    Added the skeleton source for Durandal 0.9.5

---
+++ assets/App/main.js
@@ -0,0 +1,30 @@
+﻿require.config({
+    paths: {
+        'text': 'durandal/amd/text'
+    }
+});
+
+define(function(require) {
+    var app = require('durandal/app'),
+        viewLocator = require('durandal/viewLocator'),
+        system = require('durandal/system');
+
+    require('durandal/messageBox')
+    require('viewmodels/shell')
+    require('viewmodels/welcome')
+    require('viewmodels/flickr')
+    
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.start().then(function () {
+        //If our view model is in a 'viewmodels' folder, looks for the view in a 'views' folder.
+        viewLocator.useConvention();
+
+        app.adaptToDevice();
+        
+        //Show the app by setting the root view model for our application.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
