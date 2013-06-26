4411797f0b731fc9ae28a9e53c09f61cd8598915
El Cattivo <elcattivo.r0x@gmail.com>
2013-06-26 03:19:00 +0200

    Remove unneeded goto jumps from make.bat

--- make.bat
+++ make.bat
@@ -11,36 +11,28 @@ if "%1"=="pack" goto package
 if "%1"=="package" goto package
 
 echo make: *** No rule to make target `%1'.  Stop.
-goto exit
+exit
 
 :start
     echo [x] Building assets and starting development server...
     mimosa watch -s
-    goto exit
 
 :startd
     echo [x] Cleaning compiled directory, building assets and starting development server..
     mimosa watch -sd
-    goto exit
 
 :build
     echo [x] Building assets...
     mimosa build
-    goto exit
 
 :buildo
     echo [x] Building and optimizing assets...
     mimosa build -o
-    goto exit
 
 :clean
     echo [x] Removing compiled files...
     mimosa clean
-    goto exit
 
 :pack
     echo [x] Building and packaging application...
     mimosa build -omp
-    goto exit
-
-:exit
